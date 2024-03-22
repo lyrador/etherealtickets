9500c7a617d153a86ac4e5162af2b8f9b2bc1c55
Zhou Yujie <e0726133@u.nus.edu>
2024-03-22 11:46:49 +0800

    updated Ticket.sol and Marketplace.sol

--- hardhat-repo/contracts/Ticket.sol
+++ hardhat-repo/contracts/Ticket.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.8.24;
 
+import "./Concert.sol";
+
 contract Ticket {
+    
+    Concert concertContract;
+    
     struct Ticket {
         uint256 ticketId;
-        uint256 eventId;
+        uint256 concertId;
         address prevTicketOwner;
+        string category;
+        uint256 cost; 
+        bool purchased;
     }
 
     mapping(uint256 => Ticket) public tickets;
@@ -13,13 +21,36 @@ contract Ticket {
     // reference Concert contract
     address public concertAddress;
 
-    event TicketCreated(uint256 indexed ticketId, uint256 indexed eventId, address owner);
+    event TicketCreated(uint256 indexed ticketId, uint256 indexed concertId, string category, uint256 cost, address owner);
 
     constructor(address _concertAddress) {
-        concertAddress = _concertAddress;
+        concertContract = Concert(_concertAddress);
     }
 
+    function createTicket(uint256 ticketId, uint256 concertId, string category, uint256 cost) public { 
+        require(concertContract.isValidConcert(concertId), "Concert is invalid");
+        require(concertContract.getOwner() == msg.sender, "Only the concert owner can create tickets");
+
+        // Check uniqueness of ticketId
+        require(tickets[ticketId].ticketId == 0, "Ticket already exists");
+
+        tickets[ticketId] = Ticket({
+            ticketId: ticketId,
+            concertId: concertId, 
+            prevTicketOwner: address(0), // Initially, there's no previous owner
+            category: category,
+            cost: cost,
+            purchased: false // Initially, the ticket is not purchased
+        });
+
+        ticketOwner[ticketId] = msg.sender; // Marking the ticket's creator as its initial owner
+
+        emit TicketCreated(ticketId, concertId, category, cost, msg.sender);
+    }
+
+
     function validateTicket(uint256 ticketId) public view returns (bool) {
+        // think of validating with NRIC of the buyer 
         return tickets[ticketId].ticketId == ticketId;
     }
 
